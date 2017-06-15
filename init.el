bb96e75a3f8eba8860792856e83de20a586375c7
Gilbert Consellado <=>
2017-06-15 13:26:23 +0800

    dvorak layout

--- init.el
+++ init.el
@@ -30,6 +30,8 @@ This function should only modify configuration layer settings."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     (keyboard-layout :variables
+                      kl-layout 'dvorak)
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
