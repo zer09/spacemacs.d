60a06034fad031f2cd53aa36009bb8253909cfe3
Gilbert Consellado <=>
2018-03-25 19:14:37 +0800

    USe Inconsolata Nerd Font

--- init.el
+++ init.el
@@ -252,7 +252,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Inconsolata"
+   dotspacemacs-default-font '("Inconsolata Nerd Font"
                                :size 16
                                :weight normal
                                :width normal)
