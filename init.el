64ab350b0ef9410833ff81d016d85e60f254e6e7
Gilbert Consellado <gilbertconsellado@gmail.com>
2020-03-24 15:28:27 +0800

    set custom-file file

--- init.el
+++ init.el
@@ -505,6 +505,9 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
+  (setq custom-file
+        (concat
+         (file-name-directory dotspacemacs-filepath) "customize.el"))
   (setq-default git-magit-status-fullscreen t)
   (setq-default which-key-idle-delay 0.1)
 
